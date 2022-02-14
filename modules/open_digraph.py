c803b81c5d7093f8fef342292ef9b4e272db9301
Manitas Bahri <manitas.bhr@gmail.com>
2022-02-14 21:14:06 +0100

    Implementation de open_digraph.to_str_dot_format

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -2,7 +2,7 @@ import os
 from random import random, sample
 import re
 from tkinter import N
-from urllib.parse import quote
+from urllib.parse import quote, unquote 
 
 from .utils import (random_int_matrix,
                     random_triangular_int_matrix,
@@ -725,39 +725,30 @@ Arêtes : {}""".format(", ".join([str(node) for node in self.nodes.values()]),
                 A[i][j] = node.get_child_multiplicity(child)
         return A
     
-    # def dot_format(self, verbose=False, for_web=False):
-    #     digraph = ""
+    def to_str_dot_format(self, verbose=False):
+        digraph = "digraph G{\n"
 
-    #     for node in self.get_nodes():
-    #         form = ("shape=invhouse, " if node.get_id() in self.get_input_ids() else 
-    #                    "shape=house, " if node.get_id() in self.get_output_ids() else "")
-            
-    #         id_str = f"\nid: {node.get_id()}" if verbose else ""
-
-    #         f.write(f"v{ node.get_id() } [{form}label=\"{ node.get_label() }{ repr(id_str)[1:-1] }\"];\n")
-        
-    #     for node in self.get_nodes():  
-    #         for child in node.get_children_ids():
-    #             line = f"v{node.get_id()} -> v{child};\n"
-    #             f.write(line * node.get_child_multiplicity(child))
-
-    def save_as_dot_file(self, path, verbose=False):
-        f = open(path, "w")
-        f.write("digraph G {\n")
         for node in self.get_nodes():
             form = ("shape=invhouse, " if node.get_id() in self.get_input_ids() else 
                        "shape=house, " if node.get_id() in self.get_output_ids() else "")
             
             id_str = f"\nid: {node.get_id()}" if verbose else ""
 
-            f.write(f"v{ node.get_id() } [{form}label=\"{ node.get_label() }{ repr(id_str)[1:-1] }\"];\n")
+            digraph += f"v{ node.get_id() } [{form}label=\"{ node.get_label() }{ repr(id_str)[1:-1] }\"];\n"
         
         for node in self.get_nodes():  
             for child in node.get_children_ids():
                 line = f"v{node.get_id()} -> v{child};\n"
-                f.write(line * node.get_child_multiplicity(child))
+                digraph += line * node.get_child_multiplicity(child)
+        
+        digraph += "}"
+
+        return digraph
                 
-        f.write("\n}")
+
+    def save_as_dot_file(self, path, verbose=False):
+        f = open(path, "w")
+        f.write(self.to_str_dot_format(verbose))
         f.close()
 
     @classmethod
@@ -769,9 +760,9 @@ Arêtes : {}""".format(", ".join([str(node) for node in self.nodes.values()]),
 
         graph = open_digraph.empty()
         dict_node = {}
-        # nodes = set()
+
         for line in f:
-            lbl_re = re.search(r".*\[label=\"(.*?)\"\]", line)
+            lbl_re = re.search(r".*label=\"(.*?)\"\]", line)
             lbl_node = lbl_re.group(1) if lbl_re != None else ""
 
             line = line.strip().split("[")[0]
@@ -797,23 +788,6 @@ Arêtes : {}""".format(", ".join([str(node) for node in self.nodes.values()]),
         return graph
 
     def display(self, verbose=False):
-        digraph = "digraph%7B"
-
-        for node in self.get_nodes():
-            form = ("shape%3Dinvhouse%2C " if node.get_id() in self.get_input_ids() else 
-                       "shape%3Dhouse%2C " if node.get_id() in self.get_output_ids() else "")
-            
-            id_str = f"%0A%09id%3A {node.get_id()}" if verbose else ""
-
-            digraph += f"v{ node.get_id() } %5B{form}label%3D%22{ node.get_label() }{ repr(id_str)[1:-1] }%22%5D%3B"
-        
-        for node in self.get_nodes():  
-            for child in node.get_children_ids():
-                line = f"v{node.get_id()} -%3E v{child}%3B"
-                digraph += line * node.get_child_multiplicity(child)
-        
-        digraph += "%7D"
-        
-        url = f"https://dreampuf.github.io/GraphvizOnline/#\"{digraph}\""
-
+        digraph = quote(self.to_str_dot_format(verbose))
+        url = f'https://dreampuf.github.io/GraphvizOnline/#"{digraph}"'
         os.system(f"firefox {url}")
