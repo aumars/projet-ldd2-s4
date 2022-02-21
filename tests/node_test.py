e459bcdc532a888d42ee9e4f2831df78dc295ba3
Pradana AUMARS <pradana.aumars@universite-paris-saclay.fr>
2022-02-21 23:09:39 +0100

    Implémente hypothesis dans node

--- tests/node_test.py
+++ tests/node_test.py
@@ -2,244 +2,166 @@ from modules.node import node
 import unittest
 import sys
 import os
+from hypothesis import given, strategies as st
+from collections import Counter
 root = os.path.normpath(os.path.join(__file__, './../..'))
 sys.path.append(root)  # allows us to fetch files from the project root
+
+
+@st.composite
+def node_strategy(draw, no_parents=True, no_children=True):
+    id = draw(st.integers())
+    label = draw(st.text())
+    if no_parents:
+        parents_min_size = 0
+    else:
+        parents_min_size = 1
+    if no_children:
+        children_min_size = 0
+    else:
+        children_min_size = 1
+    parents = draw(st.dictionaries(st.integers(), st.integers(min_value=1), min_size=parents_min_size))
+    children = draw(st.dictionaries(st.integers(), st.integers(min_value=1), min_size=children_min_size))
+    return node(id, label, parents, children)
 
 
 class NodeTest(unittest.TestCase):
     """Tests of the node class."""
 
-    def setUp(self):
-        self.n0 = node(1, '1', {0: 1}, {2: 1})
+    @given(id=st.integers(),
+           label=st.text(),
+           parents=st.dictionaries(st.integers(), st.integers(min_value=0)),
+           children=st.dictionaries(st.integers(), st.integers(min_value=0)))
+    def test_valid_init_node(self, id, label, parents, children):
+        """Test the constructor with valid parameters."""
+        n = node(id, label, parents, children)
+        self.assertIsInstance(n, node)
 
-    def test_init_node(self):
-        """Test the constructor."""
-        self.assertEqual(self.n0.id, 1)
-        self.assertEqual(self.n0.label, '1')
-        self.assertEqual(self.n0.parents, {0: 1})
-        self.assertEqual(self.n0.children, {2: 1})
-        self.assertIsInstance(self.n0, node)
+    @given(id=st.integers(),
+           label=st.text(),
+           parents=st.dictionaries(st.integers(), st.integers(max_value=-1), min_size=1),
+           children=st.dictionaries(st.integers(), st.integers(max_value=-1), min_size=1))
+    def test_invalid_init_node(self, id, label, parents, children):
+        """Test the constructor with invalid parameters."""
+        self.assertRaises(ValueError, node, id, label, parents, children)
 
-    def test_copy_node(self):
+    @given(node_strategy())
+    def test_copy_node(self, n):
         """Test the copy method."""
-        self.assertIsNot(self.n0.copy(), self.n0)
+        self.assertIsNot(n.copy(), n)
 
-    def test_get_id_node(self):
+    @given(node_strategy())
+    def test_get_id_node(self, n):
         """Test the get_id method."""
-        self.assertEqual(self.n0.get_id(), 1)
+        self.assertEqual(n.get_id(), n.id)
 
-    def test_get_label_node(self):
+    @given(node_strategy())
+    def test_get_label_node(self, n):
         """Test the get_label method."""
-        self.assertEqual(self.n0.get_label(), '1')
+        self.assertEqual(n.get_label(), n.label)
 
-    def test_get_parent_ids_node(self):
+    @given(node_strategy())
+    def test_get_parent_ids_node(self, n):
         """Test the get_parents_ids method."""
-        self.assertEqual(self.n0.get_parent_ids(), [0])
+        self.assertEqual(n.get_parent_ids(), list(n.parents.keys()))
 
-    def test_get_children_ids_node(self):
+    @given(node_strategy())
+    def test_get_children_ids_node(self, n):
         """Test the get_children_ids method."""
-        self.assertEqual(self.n0.get_children_ids(), [2])
+        self.assertEqual(n.get_children_ids(), list(n.children.keys()))
 
-    def test_get_parent_multiplicity_node(self):
+    @given(node_strategy(), st.integers())
+    def test_get_parent_multiplicity_node(self, n, parent_id):
         """Test the get_parent_multiplicity method."""
-        self.assertEqual(self.n0.get_parent_multiplicity(0), 1)
+        self.assertEqual(n.get_parent_multiplicity(parent_id), n.parents.get(parent_id, 0))
 
-    def test_get_parent_multiplicity_nonexistant_id_node(self):
-        """Test the get_parent_multiplicity method given an nonexistant ID."""
-        self.assertRaises(ValueError, self.n0.get_parent_multiplicity, 1)
-
-    def test_get_child_multiplicity_node(self):
+    @given(node_strategy(), st.integers())
+    def test_get_child_multiplicity_nonexistant_id_node(self, n, child_id):
         """Test the get_child_multiplicity method."""
-        self.assertEqual(self.n0.get_child_multiplicity(2), 1)
+        self.assertEqual(n.get_parent_multiplicity(child_id), n.parents.get(child_id, 0))
 
-    def test_get_child_multiplicity_nonexistant_id_node(self):
-        """Test the get_child_multiplicity method given an nonexistant ID."""
-        self.assertRaises(ValueError, self.n0.get_child_multiplicity, 1)
+    @given(node_strategy(), st.integers())
+    def test_set_id_node(self, n, id):
+        """Test the set_id method."""
+        n.set_id(id)
+        self.assertEqual(n.get_id(), id)
 
-    def test_set_id_node(self):
-        """Test the set_id method given a positive integer."""
-        self.n0.set_id(10)
-        self.assertEqual(self.n0.get_id(), 10)
+    @given(node_strategy(), st.text())
+    def test_set_label_node(self, n, label):
+        """Test the set_label method."""
+        n.set_label(label)
+        self.assertEqual(n.get_label(), label)
 
-    def test_set_negative_id_node(self):
-        """Test the set_id method given a stricly negative integer."""
-        self.n0.set_id(-1)
-        self.assertEqual(self.n0.get_id(), -1)
+    @given(node_strategy(), st.lists(st.integers()))
+    def test_set_parent_ids_node(self, n, parents):
+        """Test the set_parent_ids method."""
+        parents_count = Counter(parents)
+        n.set_parent_ids(parents)
+        self.assertCountEqual(n.get_parent_ids(), list(set(parents)))
+        for key, value in parents_count.items():
+            self.assertEqual(n.get_parent_multiplicity(key), value)
 
-    def test_set_empty_label_node(self):
-        """Test the set_label method given an empty string."""
-        self.n0.set_label('')
-        self.assertEqual(self.n0.get_label(), '')
+    @given(node_strategy(), st.lists(st.integers()))
+    def test_set_children_ids_node(self, n, children):
+        """Test the set_children_ids method."""
+        children_count = Counter(children)
+        n.set_children_ids(children)
+        self.assertCountEqual(n.get_children_ids(), list(set(children)))
+        for key, value in children_count.items():
+            self.assertEqual(n.get_child_multiplicity(key), value)
 
-    def test_set_label_node(self):
-        """Test the set_label method given a single character."""
-        self.n0.set_label('a')
-        self.assertEqual(self.n0.get_label(), 'a')
-
-    def test_set_string_label_node(self):
-        """Test the set_label method given a string of multiple characters."""
-        self.n0.set_label('aa')
-        self.assertEqual(self.n0.get_label(), 'aa')
-
-    def test_set_parent_ids_empty_list_node(self):
-        """Test the set_parent_ids method given an empty list."""
-        self.n0.set_parent_ids([])
-        self.assertEqual(self.n0.get_parent_ids(), [])
-
-    def test_set_parent_ids_node(self):
-        """Test the set_parent_ids method given a valid list."""
-        self.n0.set_parent_ids([0, 1])
-        self.assertIn(0, self.n0.get_parent_ids())
-        self.assertIn(1, self.n0.get_parent_ids())
-        self.assertEqual(self.n0.get_parent_multiplicity(0), 1)
-        self.assertEqual(self.n0.get_parent_multiplicity(1), 1)
-
-    def test_set_parent_negative_id_node(self):
-        """Test the set_parent_ids method given a list with a strictly negative
-        ID."""
-        self.n0.set_parent_ids([0, -1])
-        self.assertIn(0, self.n0.get_parent_ids())
-        self.assertIn(-1, self.n0.get_parent_ids())
-        self.assertEqual(self.n0.get_parent_multiplicity(0), 1)
-        self.assertEqual(self.n0.get_parent_multiplicity(-1), 1)
-
-    def test_set_parent_multiple_values_node(self):
-        """Test the set_parent_ids method given a list with duplicates."""
-        self.n0.set_parent_ids([0, 0, 1, 1, 0, 2])
-        self.assertIn(0, self.n0.get_parent_ids())
-        self.assertIn(1, self.n0.get_parent_ids())
-        self.assertIn(2, self.n0.get_parent_ids())
-        self.assertEqual(self.n0.get_parent_multiplicity(0), 3)
-        self.assertEqual(self.n0.get_parent_multiplicity(1), 2)
-        self.assertEqual(self.n0.get_parent_multiplicity(2), 1)
-
-    def test_set_children_ids_empty_list_node(self):
-        """Test the set_children_ids method given an empty list."""
-        self.n0.set_children_ids([])
-        self.assertEqual(self.n0.get_children_ids(), [])
-
-    def test_set_children_ids_node(self):
-        """Test the set_children_ids method given a valid list."""
-        self.n0.set_children_ids([0, 1])
-        self.assertIn(0, self.n0.get_children_ids())
-        self.assertIn(1, self.n0.get_children_ids())
-        self.assertEqual(self.n0.get_child_multiplicity(0), 1)
-        self.assertEqual(self.n0.get_child_multiplicity(1), 1)
-
-    def test_set_child_negative_id_node(self):
-        """Test the set_children_ids method given a list with a strictly negative
-        ID."""
-        self.n0.set_children_ids([0, -1])
-        self.assertIn(0, self.n0.get_children_ids())
-        self.assertIn(-1, self.n0.get_children_ids())
-        self.assertEqual(self.n0.get_child_multiplicity(0), 1)
-        self.assertEqual(self.n0.get_child_multiplicity(-1), 1)
-
-    def test_set_child_multiple_values_node(self):
-        """Test the set_children_ids method given a list with duplicates."""
-        self.n0.set_children_ids([0, 0, 1, 1, 0, 2])
-        self.assertIn(0, self.n0.get_children_ids())
-        self.assertIn(1, self.n0.get_children_ids())
-        self.assertIn(2, self.n0.get_children_ids())
-        self.assertEqual(self.n0.get_child_multiplicity(0), 3)
-        self.assertEqual(self.n0.get_child_multiplicity(1), 2)
-        self.assertEqual(self.n0.get_child_multiplicity(2), 1)
-        
-    def test_add_parent_id_node(self):
+    @given(node_strategy(), st.integers())
+    def test_add_parent_id_node(self, n, parent_id):
         """Test the add_parent_id method."""
-        self.n0.add_parent_id(10)
-        self.assertIn(10, self.n0.get_parent_ids())
-        self.assertEqual(self.n0.get_parent_multiplicity(10), 1)
+        m = n.get_parent_multiplicity(parent_id)
+        n.add_parent_id(parent_id)
+        self.assertEqual(n.get_parent_multiplicity(parent_id), m + 1)
 
-    def test_add_parent_existing_id_node(self):
-        """Test the add_parent_id method given an existing parent ID."""
-        self.n0.add_parent_id(0)
-        self.assertIn(0, self.n0.get_parent_ids())
-        self.assertEqual(self.n0.get_parent_multiplicity(0), 2)
-
-    def test_add_parent_negative_id_node(self):
-        """Test the add_parent_id method given a strictly negative ID."""
-        self.n0.add_parent_id(-1)
-        self.assertIn(-1, self.n0.get_parent_ids())
-        self.assertEqual(self.n0.get_parent_multiplicity(-1), 1)
-
-    def test_add_child_id_unique_id_node(self):
+    @given(node_strategy(), st.integers())
+    def test_add_child_id_node(self, n, child_id):
         """Test the add_child_id method."""
-        self.n0.add_child_id(10)
-        self.assertIn(10, self.n0.get_children_ids())
-        self.assertEqual(self.n0.get_child_multiplicity(10), 1)
+        m = n.get_child_multiplicity(child_id)
+        n.add_child_id(child_id)
+        self.assertEqual(n.get_child_multiplicity(child_id), m + 1)
 
-    def test_add_child_existing_id_node(self):
-        """Test the add_child_id method given an existing child ID."""
-        self.n0.add_child_id(2)
-        self.assertIn(2, self.n0.get_children_ids())
-        self.assertEqual(self.n0.get_child_multiplicity(2), 2)
+    @given(node_strategy(), st.integers())
+    def test_remove_parent_once_node(self, n, parent_id):
+        """Test the remove_parent_once method."""
+        m = n.get_parent_multiplicity(parent_id)
+        n.remove_parent_once(parent_id)
+        if m > 0:
+            m = m - 1
+        self.assertEqual(n.get_parent_multiplicity(parent_id), m)
 
-    def test_add_child_negative_id_node(self):
-        """Test the add_child_id method given a strictly negative ID."""
-        self.n0.add_child_id(-1)
-        self.assertIn(-1, self.n0.get_children_ids())
-        self.assertEqual(self.n0.get_child_multiplicity(-1), 1)
+    @given(node_strategy(), st.integers())
+    def test_remove_child_once_node(self, n, child_id):
+        """Test the remove_child_once method."""
+        m = n.get_child_multiplicity(child_id)
+        n.remove_child_once(child_id)
+        if m > 0:
+            m = m - 1
+        self.assertEqual(n.get_child_multiplicity(child_id), m)
 
-    def test_remove_parent_once_existing_id_node(self):
-        """Test the remove_parent_once method given an existing parent ID"""
-        self.n0.remove_parent_once(0)
-        self.assertEqual(self.n0.get_parent_ids(), [])
+    @given(node_strategy(), st.integers())
+    def test_remove_parent_id_node(self, n, parent_id):
+        """Test the remove_parent_id method."""
+        n.remove_parent_id(parent_id)
+        self.assertNotIn(parent_id, n.get_parent_ids())
 
-    def test_remove_parent_once_existing_id_multiplicity_two_node(self):
-        """Test the remove_parent_once method given an existing parent ID
-        with multiplicity 2"""
-        self.n0.add_parent_id(0)
-        self.n0.remove_parent_once(0)
-        self.assertEqual(self.n0.get_parent_ids(), [0])
+    @given(node_strategy(), st.integers())
+    def test_remove_child_id_node(self, n, child_id):
+        """Test the remove_child_id method."""
+        n.remove_child_id(child_id)
+        self.assertNotIn(child_id, n.get_children_ids())
 
-    def test_remove_parent_once_nonexistant_id_node(self):
-        """Test the remove_parent_once with an nonexistant ID"""
-        self.assertRaises(ValueError, self.n0.remove_parent_once, 1)
+    @given(node_strategy())
+    def test_indegree_node(self, n):
+        self.assertEqual(n.indegree(), sum(n.parents.values()))
 
-    def test_remove_child_once_existing_id_node(self):
-        """Test the remove_child_once method given an existing child ID"""
-        self.n0.remove_child_once(2)
-        self.assertEqual(self.n0.get_children_ids(), [])
+    @given(node_strategy())
+    def test_outdegree_node(self, n):
+        self.assertEqual(n.outdegree(), sum(n.children.values()))
 
-    def test_remove_child_once_existing_id_multiplicity_two_node(self):
-        """Test the remove_child_once method given an existing child ID
-        with multiplicity 2"""
-        self.n0.add_child_id(2)
-        self.n0.remove_child_once(2)
-        self.assertEqual(self.n0.get_children_ids(), [2])
-
-    def test_remove_child_once_nonexistant_id_node(self):
-        """Test the remove_child_once with an nonexistant ID"""
-        self.assertRaises(ValueError, self.n0.remove_child_once, 1)
-
-    def test_remove_parent_id_existant_id_node(self):
-        """Test the remove_parent_id method given an existing parent ID
-        with multiplicity 2"""
-        self.n0.add_parent_id(0)
-        self.n0.remove_parent_id(0)
-        self.assertEqual(self.n0.get_parent_ids(), [])
-
-    def test_remove_parent_id_nonexistant_id_node(self):
-        """Test the remove_parent_once with an nonexistant ID"""
-        self.assertRaises(ValueError, self.n0.remove_parent_id, 1)
-
-    def test_remove_child_id_existant_id_node(self):
-        """Test the remove_child_id method given an existing child ID
-        with multiplicity 2"""
-        self.n0.add_child_id(2)
-        self.n0.remove_child_id(2)
-        self.assertEqual(self.n0.get_children_ids(), [])
-
-    def test_remove_child_id_nonexistant_id_node(self):
-        """Test the remove_child_once with an nonexistant ID"""
-        self.assertRaises(ValueError, self.n0.remove_child_id, 1)
-
-    def test_indegree_node(self):
-        self.assertEqual(self.n0.indegree(), 1)
-
-    def test_outdegree_node(self):
-        self.assertEqual(self.n0.outdegree(), 1)
-
-    def test_degree_node(self):
-        self.assertEqual(self.n0.degree(), 2)
+    @given(node_strategy())
+    def test_degree_node(self, n):
+        self.assertEqual(n.degree(), sum(n.parents.values()) + sum(n.children.values()))
