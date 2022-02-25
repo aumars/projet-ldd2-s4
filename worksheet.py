04ef41bb1f34920011927247f17f2ad118918ff2
Manitas Bahri <manitas.bhr@gmail.com>
2022-02-25 19:40:33 +0100

    Ajout d'un graphe à deux composantes

--- worksheet.py
+++ worksheet.py
@@ -13,6 +13,27 @@ G = open_digraph([3, 4], [5, 6], [n0, n1, n2,
                                   i0, i1, 
                                   o0, o1])
 
-G.save_as_dot_file("graph.dot")
-G2 = open_digraph.from_dot_file(path="graph.dot")
-G2.display()
+
+i0 = node(0, "i0", {}, {6: 1})
+i1 = node(1, "i1", {}, {4: 1})
+i2 = node(2, "i2", {}, {4: 1})
+i3 = node(3, "i3", {}, {9: 1})
+
+n0 = node(4, "a", {1: 1, 2: 1}, {5: 1})
+n1 = node(5, "b", {4: 1}, {7: 1, 8: 1})
+n2 = node(6, "g", {0: 1}, {7: 1})
+n3 = node(7, "c", {5: 1, 6: 1}, {11: 1})
+n4 = node(8, "f", {5: 1}, {12: 1})
+n5 = node(9, "d", {3: 1}, {10: 1})
+n6 = node(10, "e", {9: 1}, {13: 1})
+
+o0 = node(11, "o0", {7: 1}, {})
+o1 = node(12, "o1", {8: 1}, {})
+o2 = node(13, "o2", {10: 1}, {})
+
+G2 = open_digraph([0, 1, 2, 3], [11, 12, 13], [i0, i1, i2, i3,
+                                              n0, n1, n2, n3, n4, n5, n6,
+                                              o0, o1, o2])
+
+
+G.display()
