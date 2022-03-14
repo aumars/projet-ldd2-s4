f1c8edbbec478b1debf4d87cb9c44a2fea41cede
Pradana AUMARS <pradana.aumars@universite-paris-saclay.fr>
2022-03-14 21:02:49 +0100

    Mis à jour documentation + améliore icompose

--- modules/open_digraph_mx/op_compositions_mx.py
+++ modules/open_digraph_mx/op_compositions_mx.py
@@ -21,7 +21,7 @@ class op_compositions_mx:
 
     def parallel(self, list_graph):
         """
-        Add graphs parallel to itself.
+        Add graphs parallel to itself. This functionality only accepts well-formed graphs.
 
         Parameters
         ----------
@@ -31,47 +31,40 @@ class op_compositions_mx:
         Returns
         ------
         open_digraph
-           The graph of the fusion of parallel graphs. 
+           The graph of the fusion of parallel graphs.
         """ 
         graph = self.empty()
         graph.iparallel([self] + list_graph)
         return graph
-    
+
     def icompose(self, g):
         """
-        Compose graph in sequence.
-        The graph g is not modified.
+        Compose graph in sequence. The graph g is not modified. This functionality only accepts well-formed graphs.
 
         Parameters
         ----------
         g : open_digraph
            A graph to compose in sequence.
-        
+
         Raises
         ------
         ValueError
             If the number of outputs in self do not coincide with the number of inputs in g.
-
         """
         if len(self.get_output_ids()) != len(g.get_input_ids()):
             raise ValueError("The number of outputs in self do not coincide with the number of inputs in g.")
-        
         self.separate_indices(g)
         self.nodes.update(g.get_id_node_map())
-        
-        for i in range(len(self.get_input_ids())):
-            o_id = self.get_node_by_id(self.get_output_ids()[i])
-            id_parent_o = o_id.get_parent_ids()[0]
-            
-            self.remove_node_by_id(o_id)
-            self.add_edge(id_parent_o, g.get_input_ids()[i])
-        
+        for outid, inid in zip(self.get_output_ids(), g.get_input_ids()):
+            onode = self.get_node_by_id(outid)
+            onode_parent_id = onode.get_parent_ids()[0] # since output node only has 1 parent
+            self.remove_node_by_id(outid)
+            self.add_edge(onode_parent_id, inid)
         self.set_output_ids(g.get_output_ids())
-    
+
     def compose(self, g):
         """
-        Get the composition of the self graph with the graph g.
-        The graphs are not modified.
+        Get the composition of the self graph with the graph g. The graphs are not modified. This functionality only accepts well-formed graphs.
 
         Parameters
         ----------
