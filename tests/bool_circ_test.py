cdaa69b1f65d082103636115fbe7a2f58ca08d99
Pradana AUMARS <pradana.aumars@universite-paris-saclay.fr>
2022-05-01 17:34:01 +0200

    bool_circ: Update tests.

--- tests/bool_circ_test.py
+++ tests/bool_circ_test.py
@@ -3,6 +3,7 @@ from modules.bool_circ import bool_circ
 import unittest
 import sys
 import os
+from hypothesis import given, strategies as st
 root = os.path.normpath(os.path.join(__file__, './../..'))
 sys.path.append(root)  # allows us to fetch files from the project root
 
@@ -30,17 +31,11 @@ class Bool_CircTest(unittest.TestCase):
         self.id8 = self.B.add_output_node(self.id7)
 
         self.C = self.B.copy()
-        self.id_copy = self.C.add_node('')
-        self.id_xor = self.C.add_node('^')
-        self.C.remove_edge(self.id0, self.id4)
-        self.C.add_edge(self.id0, self.id_copy)
-        self.C.add_edge(self.id_copy, self.id_xor)
-        self.C.add_edge(self.id_copy, self.id4)
-        self.C.add_edge(self.id3, self.id_xor)
-        self.C.add_edge(self.id_xor, self.id7)
+        self.C.get_node_by_id(self.id4).set_label('^')
 
         self.D = self.B.copy()
         self.id_extra0 = self.D.add_input_node(self.id5)
+        self.D.get_node_by_id(self.id_extra0).set_label('0')
 
         self.LEGAL_LABELS = set(['0', '1', '&', '|', '~', "~", ''])
 
@@ -140,15 +135,41 @@ class Bool_CircTest(unittest.TestCase):
         self.assertEqual(10, len(B10.get_input_ids()))
         self.assertEqual(10, len(B10.get_output_ids()))
 
-    def test_adder(self):
-        self.assertTrue(bool_circ.adder(0).is_well_formed())
-        self.assertTrue(bool_circ.adder(1).is_well_formed())
-        self.assertTrue(bool_circ.adder(4).is_well_formed())
+    def test_adder0(self):
+        A0 = bool_circ.adder(0)
+        self.assertEqual(3, len(A0.get_input_ids()))
+        self.assertEqual(2, len(A0.get_output_ids()))
+        self.assertTrue(A0.is_well_formed())
 
-    def test_half_adder(self):
-        self.assertTrue(bool_circ.half_adder(0).is_well_formed())
-        self.assertTrue(bool_circ.half_adder(1).is_well_formed())
-        self.assertTrue(bool_circ.half_adder(4).is_well_formed())
+    def test_adder1(self):
+        A1 = bool_circ.adder(1)
+        self.assertEqual(5, len(A1.get_input_ids()))
+        self.assertEqual(3, len(A1.get_output_ids()))
+        self.assertTrue(A1.is_well_formed())
+
+    def test_adder2(self):
+        A2 = bool_circ.adder(2)
+        self.assertEqual(9, len(A2.get_input_ids()))
+        self.assertEqual(5, len(A2.get_output_ids()))
+        self.assertTrue(A2.is_well_formed())
+
+    def test_half_adder0(self):
+        HA0 = bool_circ.half_adder(0)
+        self.assertEqual(2, len(HA0.get_input_ids()))
+        self.assertEqual(2, len(HA0.get_output_ids()))
+        self.assertTrue(HA0.is_well_formed())
+
+    def test_half_adder1(self):
+        HA1 = bool_circ.half_adder(1)
+        self.assertEqual(4, len(HA1.get_input_ids()))
+        self.assertEqual(3, len(HA1.get_output_ids()))
+        self.assertTrue(HA1.is_well_formed())
+
+    def test_half_adder2(self):
+        HA2 = bool_circ.half_adder(2)
+        self.assertEqual(8, len(HA2.get_input_ids()))
+        self.assertEqual(5, len(HA2.get_output_ids()))
+        self.assertTrue(HA2.is_well_formed())
 
     def test_register(self):
         g = bool_circ.register(8, 11)
@@ -187,12 +208,29 @@ class Bool_CircTest(unittest.TestCase):
         self.assertIn(self.id4, self.B.get_node_ids())
         self.assertTrue(self.B.is_well_formed())
 
-    def test_trans_not_valid(self):
+    def test_trans_not_valid0(self):
+        self.B.get_node_by_id(self.id1).set_label('0')
+        self.B.get_node_by_id(self.id3).set_label('~')
+        self.B.trans_not([self.id1])
+        self.assertNotIn(self.id1, self.B.get_node_ids())
+        self.assertIn(self.id3, self.B.get_node_ids())
+        self.assertEqual('1', self.B.get_node_by_id(self.id3).get_label())
+        self.assertTrue(self.B.is_well_formed())
+
+    def test_trans_not_valid1(self):
         self.B.get_node_by_id(self.id3).set_label('~')
         self.B.trans_not([self.id1])
         self.assertNotIn(self.id1, self.B.get_node_ids())
         self.assertIn(self.id3, self.B.get_node_ids())
         self.assertEqual('0', self.B.get_node_by_id(self.id3).get_label())
+        self.assertTrue(self.B.is_well_formed())
+
+    def test_trans_not_invalid(self):
+        self.B.trans_not([self.id0])
+        self.assertIn(self.id0, self.B.get_node_ids())
+        self.assertIn(self.id4, self.B.get_node_ids())
+        self.assertEqual('0', self.B.get_node_by_id(self.id0).get_label())
+        self.assertEqual('&', self.B.get_node_by_id(self.id4).get_label())
         self.assertTrue(self.B.is_well_formed())
 
     def test_trans_and_valid0(self):
@@ -217,110 +255,91 @@ class Bool_CircTest(unittest.TestCase):
         self.assertTrue(self.B.is_well_formed())
 
     def test_trans_xor_valid0(self):
-        self.B.trans_xor([self.id0])
-        self.assertTrue(self.B.is_well_formed())
+        self.C.trans_xor([self.id0])
+        self.assertNotIn(self.id0, self.C.get_node_ids())
+        self.assertTrue(self.C.is_well_formed())
 
     def test_trans_xor_valid1(self):
-        g = bool_circ.empty()
-        id0 = g.add_node('0')
-        id1 = g.add_node('1')
-        id2 = g.add_node('~')
-        g.add_edge(id0, id2)
-        g.add_edge(id1, id2)
-        g.trans_xor([id0])
-        self.assertNotIn(id0, g.get_node_ids())
-        self.assertIn(id1, g.get_node_ids())
-        self.assertIn(id2, g.get_node_ids())
-
-        g.trans_xor([id1])
-        self.assertNotIn(id1, g.get_node_ids())
-        self.assertIn(id2, g.get_node_ids())
-        self.assertEqual(len(g.get_node_by_id(id2).get_children_ids()), 1)
+        node4 = self.C.get_node_by_id(self.id4)
+        self.C.get_node_by_id(self.id0).set_label('1')
+        self.C.trans_xor([self.id0])
+        self.assertTrue(self.C.is_well_formed())
+        self.assertNotIn(self.id0, self.C.get_node_ids())
+        self.assertEqual(1, node4.indegree())
+        self.assertEqual(1, node4.outdegree())
+        self.assertNotIn(7, node4.get_children_ids())
+        node4_child = self.C.get_node_by_id(node4.get_children_ids()[0])
+        self.assertEqual('~',  node4_child.get_label())
+        self.assertEqual(1, node4_child.outdegree())
+        self.assertIn(self.id7, node4_child.get_children_ids())
 
     def test_trans_neutre(self):
         self.B.trans_neutral([self.id0])
         self.assertTrue(self.B.is_well_formed())
 
     def test_transform_all(self):
-        A0 = bool_circ.adder(0)
-        A0.get_node_by_id(A0.get_input_ids()[0]).set_label('1')
-        A0.get_node_by_id(A0.get_input_ids()[1]).set_label('1')
-        A0.get_node_by_id(A0.get_input_ids()[2]).set_label('0')
+        A0 = bool_circ.half_adder(0)
+        A0.set_input_bits("11")
         A0.transform_all()
         self.assertTrue(A0.is_well_formed())
 
     def test_transform_full(self):
-        A0 = bool_circ.adder(0)
-        A0.get_node_by_id(A0.get_input_ids()[0]).set_label('1')
-        A0.get_node_by_id(A0.get_input_ids()[1]).set_label('1')
-        A0.get_node_by_id(A0.get_input_ids()[2]).set_label('0')
+        A0 = bool_circ.half_adder(0)
+        A0.set_input_bits("11")
+        outputs = len(A0.get_output_ids())
         while len(A0.get_output_ids()) != len(A0.get_node_ids()):
             A0.transform_all()
             self.assertTrue(A0.is_well_formed())
+            self.assertEqual(outputs, len(A0.get_output_ids()))
 
-    def test_evaluate(self):
+    def test_evaluate_adder(self):
         A0 = bool_circ.adder(0)
-        A0.get_node_by_id(A0.get_input_ids()[0]).set_label('0')
-        A0.get_node_by_id(A0.get_input_ids()[1]).set_label('0')
-        A0.get_node_by_id(A0.get_input_ids()[2]).set_label('0')
+
+        A0.set_input_bits("000")
         self.assertEqual("00", A0.evaluate())
 
-        A0.get_node_by_id(A0.get_input_ids()[0]).set_label('1')
-        A0.get_node_by_id(A0.get_input_ids()[1]).set_label('0')
-        A0.get_node_by_id(A0.get_input_ids()[2]).set_label('0')
+        A0.set_input_bits("001")
         self.assertEqual("01", A0.evaluate())
 
-        #A0.get_node_by_id(A0.get_input_ids()[0]).set_label('0')
-        #A0.get_node_by_id(A0.get_input_ids()[1]).set_label('1')
-        #A0.get_node_by_id(A0.get_input_ids()[2]).set_label('0')
-        #self.assertEqual("01", A0.evaluate())
+        A0.set_input_bits("010")
+        self.assertEqual("01", A0.evaluate())
 
-        A0.get_node_by_id(A0.get_input_ids()[0]).set_label('1')
-        A0.get_node_by_id(A0.get_input_ids()[1]).set_label('1')
-        A0.get_node_by_id(A0.get_input_ids()[2]).set_label('0')
+        A0.set_input_bits("011")
+        self.assertEqual("10", A0.evaluate())
+
+        A0.set_input_bits("100")
+        self.assertEqual("01", A0.evaluate())
+
+        A0.set_input_bits("101")
+        self.assertEqual("10", A0.evaluate())
+
+        A0.set_input_bits("110")
+        self.assertEqual("10", A0.evaluate())
+
+        A0.set_input_bits("111")
         self.assertEqual("11", A0.evaluate())
-        #A8 = bool_circ.adder(8)
-        #A8.evaluate()
-        #self.assertTrue(A8.is_well_formed())
 
-    @unittest.skip
-    def test_add_bool_circ(self):
-        r_bad = [0, 1, 0, 1, 0]
-        r0 = [0] * 8
-        r1 = [1] * 8
+    def test_evaluate_half_adder(self):
+        A0 = bool_circ.half_adder(0)
 
-        self.assertRaises(ValueError, bool_circ.adder, r_bad, r1)
-        self.assertRaises(ValueError, bool_circ.adder, r1, r_bad)
+        A0.set_input_bits("00")
+        self.assertEqual("00", A0.evaluate())
 
-        sum0, carry = bool_circ.adder(r0, r0)
-        self.assertListEqual(sum0, r0)
-        self.assertListEqual(carry, 0)
+        A0.set_input_bits("01")
+        self.assertEqual("01", A0.evaluate())
 
-        sum1, carry = bool_circ.adder(r0, r1)
-        self.assertListEqual(sum1, r1)
-        self.assertListEqual(carry, 1)
+        A0.set_input_bits("10")
+        self.assertEqual("01", A0.evaluate())
 
-        sum1, carry = bool_circ.adder(r0, r1)
-        self.assertListEqual(sum1, r1)
-        self.assertListEqual(carry, 1)
+        A0.set_input_bits("11")
+        self.assertEqual("10", A0.evaluate())
 
-    @unittest.skip
-    def test_half_add_bool_circ(self):
-        r_bad = [0, 1, 0, 1, 0]
-        r0 = [0] * 8
-        r1 = [1] * 8
-
-        self.assertRaises(ValueError, bool_circ.half_adder, r_bad, r1)
-        self.assertRaises(ValueError, bool_circ.half_adder, r1, r_bad)
-
-        sum0, carry = bool_circ.half_adder(r0, r0)
-        self.assertListEqual(sum0, r0)
-        self.assertListEqual(carry, 0)
-
-        sum1, carry = bool_circ.half_adder(r0, r1)
-        self.assertListEqual(sum1, r1)
-        self.assertListEqual(carry, 1)
-
-        sum1, carry = bool_circ.half_adder(r0, r1)
-        self.assertListEqual(sum1, r1)
-        self.assertListEqual(carry, 1)
+    @given(st.integers(min_value=0, max_value=10),
+           st.integers(min_value=0, max_value=10))
+    def test_add(self, a, b):
+        c = a + b
+        res, overflow, bits = bool_circ.add(a, b)
+        if overflow:
+            self.assertEqual(res, c % bits)
+        else:
+            self.assertEqual(c, res)
